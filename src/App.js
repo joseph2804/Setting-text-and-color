4b630d536e9dbc3046cfee11d31ecca33c6254b7
joseph <phamhaiduongq12@gmail.com>
2019-11-27 13:50:13 +0700

    first commit
    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+        <div className="container">
+            <div className="row no-gutters">
+              <div className="col-6">
+                  <div className="title">
+                      <h6>Color Picker</h6>
+                  </div>
+                  <button type="button" className="red bt"></button>
+                  <button type="button" className="yellow bt"></button>
+                  <button type="button" className="green bt"></button>
+                  <button type="button" className="gray bt"></button>
+              </div>
+              <div className="col-6">
+                  <div className="title">
+                      <h6>Font Size: </h6>
+                  </div>
+                  <button type="button" className="up">Up</button>
+                  <button type="button" className="down">Down</button>
+              </div>
+            </div>
+            <div className="row no-gutters justify-content-center ">
+                <div className="col-4">
+                    <h6 className="h6"><button className="reset">Reset</button></h6>
+                </div>
+            </div>
+            <div className="row no-gutters justify-content-center">
+                <div className="col-12">
+                    <div className="announcement title">Color: - Fontsize: </div>
+                    <div className="text-content">Color and Fontsize are picked</div>
+                </div>
+            </div>
+        </div>
+    );
+    
+}
 }
 
 export default App;
---
+++ src/components/PickColor.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class PickColor extends React.Component {
+    render() {
+        return(
+            <div>
+                <div>
+                    <h6>Color Picker</h6>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <button></button>
+                        </li>
+                        <li>
+                            <button></button>
+                        </li>
+                        <li>
+                            <button></button>
+                        </li>
+                        <li>
+                            <button></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+        
+    }
+}
+
+export default PickColor
